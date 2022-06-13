a44088322492bd51e5fff36f62cc8a972a13f535
ito <kyosuke.ito@gmail.com>
2022-06-13 14:49:11 -0700

    create sample fetch function by using RTK

--- src/index.tsx
+++ src/index.tsx
@@ -2,16 +2,16 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-// import { store } from './store';
-// import { Provider } from 'react-redux';
+import { store } from 'RTK/app/store';
+import { Provider } from 'react-redux';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
   <React.StrictMode>
-    {/* <Provider store={store}> */}
-    <App />
-    {/* </Provider> */}
+    <Provider store={store}>
+      <App />
+    </Provider>
   </React.StrictMode>
 );
