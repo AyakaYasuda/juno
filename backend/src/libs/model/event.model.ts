af12da92174552374e5b78420c282345c2a12754
SOunit <workbookwork7@gmail.com>
2022-06-14 18:45:42 -0700

    refactor update logic

--- backend/src/libs/model/event.model.ts
+++ backend/src/libs/model/event.model.ts
@@ -12,7 +12,11 @@ import { v4 } from 'uuid';
 
 import DbModel from './dbModel';
 import { IEventUserIsAttending } from '@libs/types/eventUserIsAttending.type';
+import { APIGatewayProxyResultV2 } from 'aws-lambda';
+import { UpdateEventReqBody } from '@libs/requests/UpdateEventReqBody';
 
+// FIXME: move validator to service class
+// model class should focus only on CRUD
 class EventModel extends DbModel {
   constructor() {
     super();
@@ -119,6 +123,48 @@ class EventModel extends DbModel {
     };
 
     await this.put(EventUserIsAttendingParams);
+  }
+
+  public async errorIfEventNotExist(
+    eventId: string,
+    eventNotExistErrorMessage: string
+  ): Promise<APIGatewayProxyResultV2<any>> {
+    const params = {
+      TableName: tableNames.USER_EVENT,
+      Key: {
+        PK: 'event',
+        SK: eventId,
+      },
+    };
+
+    const eventResponseData = await this.get(params);
+
+    if (Object.keys(eventResponseData).length === 0) {
+      throw new HttpError(404, eventNotExistErrorMessage);
+    }
+
+    return eventResponseData.Item;
+  }
+
+  public async updateEvent(
+    eventId: string,
+    eventData: any,
+    reqBody: UpdateEventReqBody
+  ) {
+    const updatedEvent = {
+      ...eventData,
+      ...reqBody,
+      PK: 'event',
+      SK: eventId,
+    };
+
+    const params = {
+      TableName: tableNames.USER_EVENT,
+      Item: updatedEvent,
+    };
+
+    const result = await this.put(params);
+    console.log('result', result);
   }
 }
 
