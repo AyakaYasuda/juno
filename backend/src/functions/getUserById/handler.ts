576e75702a9b3ab0ae0f13f0bffc977e63f24935
Ayaka Yasuda <ayakayasuda23@gmail.com>
2022-06-15 16:31:20 -0700

    removed unnesessary code from getUserById

--- backend/src/functions/getUserById/handler.ts
+++ backend/src/functions/getUserById/handler.ts
@@ -1,23 +1,7 @@
 import { APIGatewayProxyEvent, APIGatewayProxyResultV2 } from 'aws-lambda';
-import { formatJSONResponse, HttpError, handleError } from '@libs/api-gateway';
+import { formatJSONResponse, handleError } from '@libs/api-gateway';
 import { middyfy } from '@libs/lambda';
 import UserServices from '@libs/services/user.services';
-
-const AWS = require('aws-sdk');
-const dynamodb = new AWS.DynamoDB.DocumentClient();
-const USER_EVENT_TABLE = 'user-event';
-
-interface IUser {
-  PK: string;
-  SK: string;
-  firstName: string;
-  lastName: string;
-  email: string;
-  password: string;
-  isAdmin: boolean;
-  message: string;
-  allergy: string;
-}
 
 const getUserById = async (
   event: APIGatewayProxyEvent
