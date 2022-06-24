8e311efc81cb8ecfbdc744bf5221b776c00101eb
SOunit <workbookwork7@gmail.com>
2022-06-24 13:05:48 -0700

    fix auth-page-layout component

--- src/views/components/molecules/Layout/AuthPageLayout.tsx
+++ src/views/components/molecules/Layout/AuthPageLayout.tsx
@@ -8,18 +8,20 @@ type Props = {
 
 const AuthPageLayout: React.FC<Props> = ({ children }) => {
   return (
-    <section className="flex sm:relative">
-      <div className="w-full h-screen md:w-1/2">
-        <img
-          src={topImg}
-          alt="top-img"
-          className="w-full h-full object-cover"
-        />
-      </div>
-      <div className="absolute top-0 left-0 w-full h-screen FlexCenter bg-Pink-default bg-opacity-30 md:bg-gradient-to-b md:from-Pink-lighter md:to-Pink-default md:w-1/2 md:relative">
-        <div className="w-3/4 h-4/5 text-center flex flex-col">
-          {children}
-          <TempCopyright className="absolute bottom-4 left-1/2 -translate-x-1/2" />
+    <section className="flex flex-col h-full relative bg-Pink-default bg-opacity-30 md:bg-gradient-to-b md:from-Pink-lighter md:to-Pink-default">
+      <div className="flex sm:relative">
+        <div className="w-full h-screen md:w-1/2">
+          <img
+            src={topImg}
+            alt="top-img"
+            className="w-full h-full object-cover"
+          />
+        </div>
+        <div className="min-h-screen flex-col absolute top-0 left-0 w-full FlexCenter md:w-1/2 md:relative">
+          <div className="w-3/4 text-center flex flex-col mb-10">
+            {children}
+          </div>
+          <TempCopyright className="bottom-4 absolute" />
         </div>
       </div>
     </section>
